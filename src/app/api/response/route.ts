fd6c55a29d26cafcdb8eeb59348d534eb91ff113
Tusharjamdade <tusharnjamdade@gmail.com>
2024-11-28 14:14:28 +0000

    init project

---
+++ src/app/api/response/route.ts
@@ -0,0 +1,19 @@
+// const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextRequest } from "next/server";
+
+export async function POST(req:NextRequest) {
+    const header = await req.json();
+    console.log(header.search)
+    const API_KEY:string = process.env.GEMINI_API_KEY || "";
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    
+    const prompt = header.search + ".Answer in 4 lines";
+    
+    const result = await model.generateContent(prompt);
+    const responseText = result.response.text()
+    return Response.json({
+        responseText
+    })
+}
