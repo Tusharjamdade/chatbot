81545a3a0ae7f54070cf045752f26ebf1ed59e00
Tusharjamdade <tusharnjamdade@gmail.com>
2024-11-28 14:45:43 +0000

    added a condition to check words

--- src/app/api/response/route.ts
+++ src/app/api/response/route.ts
@@ -4,13 +4,14 @@ import { NextRequest } from "next/server";
 
 export async function POST(req:NextRequest) {
     const header = await req.json();
-    console.log(header.search)
+    const search = header.search;
     const API_KEY:string = process.env.GEMINI_API_KEY || "";
     const genAI = new GoogleGenerativeAI(API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-    
-    const prompt = header.search + ".Answer in 4 lines";
-    
+    let prompt = search;
+    if(search.length > 10){
+        prompt = search  + ".Answer in 4 lines";
+    }
     const result = await model.generateContent(prompt);
     const responseText = result.response.text()
     return Response.json({
